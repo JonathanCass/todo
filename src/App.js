21218894d45aac09aed48d73c80bc03848de2468
JonCass <JCass1980@gmail.com>
2017-03-23 14:23:52 -0700

    moving css to file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import './App.css'
 
-var App = React.createClass({
+var App=React.createClass({
   getInitialState: function() {
     return {
-      count : 0
+      itemsList : [
+        'Wake Up',
+        'Brush Teeth',
+        'Pet Bird',
+        'Exercise'
+      ]
     }
   },
   render: function() {
     return (
       <div className="App">
         <form className="inputForm">
-          <input type ="text" className ="inputBox" placeholder ="Enter Item To Do Here"/><br />
+          <input type="text" className="inputBox" placeholder="Enter Item To Do Here"/><br />
         </form>
+          <ul>
+            {this.state.itemsList.map(function(item){
+            return <li>{item}</li>                  
+          })}
+        </ul>
       </div>
     )
   }
