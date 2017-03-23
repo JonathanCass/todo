ecb0481e9c339f09277ab17011b620ce6842d3d8
JonCass <JCass1980@gmail.com>
2017-03-23 14:40:02 -0700

    adding in submit code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react'
 import './App.css'
 
+var styles = {
+  input: {
+    fontSize: 14,
+    color: 'red',
+    height: 40,
+    width: 400 
+  },
+  centerBox: {
+    position: 'fixed',
+    top: '20%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
 var App=React.createClass({
   getInitialState: function() {
     return {
@@ -14,9 +28,9 @@ var App=React.createClass({
   },
   render: function() {
     return (
-      <div className="App">
+      <div className="App" style={styles.centerBox}>
         <form className="inputForm">
-          <input type="text" className="inputBox" placeholder="Enter Item To Do Here"/><br />
+          <input style={styles.input} type="text" className="inputBox" placeholder="Enter Item To Do Here"/><br />
         </form>
           <ul>
             {this.state.itemsList.map(function(item){
